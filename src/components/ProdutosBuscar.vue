7bd409f005d177e485f5600218fad2774bf1f3e8
Thiago Silva <thicsilva@yahoo.com.br>
2019-07-25 17:10:20 -0300

    Estilizacao dos produtos na home

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -5,12 +5,13 @@
       name="busca"
       id="busca"
       v-model="busca"
+      placeholder="Buscar..."
     >
     <button
       type="submit"
       id="lupa"
       @click.prevent="buscarProdutos"
-    >Buscar</button>
+    ></button>
 
   </form>
 </template>
@@ -31,5 +32,33 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#busca:focus,
+#busca:hover {
+  transform: scale(1.1);
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+}
 </style>
