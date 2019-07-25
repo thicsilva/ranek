4e0a36a6b39ff30b906ee5f8fad8662e1470f0fb
Thiago Silva <thicsilva@yahoo.com.br>
2019-07-25 15:27:35 -0300

    Criado lista de produtos usando axios

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,10 +1,40 @@
 <template>
-  <p>Lista</p>
+  <section class="produtos-container">
+    <div
+      v-for="produto in produtos"
+      :key="produto.id"
+    >
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        :alt="produtos.fotos[0].titulo"
+      >
+      <p class="preco">{{produto.preco}}</p>
+      <h2 class="titulo">{{produto.nome}}</h2>
+      <p>{{produto.descricao}}</p>
+    </div>
+  </section>
 </template>
 
 <script>
+import { api } from '@/services.js';
 export default {
-  name: "ProdutosLista"
+  name: "ProdutosLista",
+  data () {
+    return {
+      produtos: null
+    }
+  },
+  methods: {
+    getProdutos () {
+      api.get('/produto').then(r => {
+        this.produtos = r.data;
+      })
+    }
+  },
+  created () {
+    this.getProdutos();
+  },
 }
 </script>
 
