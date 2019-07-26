f825066ab4896a5d5a08a067bab94c9be41f4cea
Thiago Silva <thicsilva@yahoo.com.br>
2019-07-26 14:35:23 -0300

    Criado formulário de usuário e configurado através de mutations

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -1,12 +1,47 @@
 <template>
-  <p>Criar login</p>
+  <section>
+    <h2>Crie a sua conta</h2>
+    <transition mode="out-in">
+
+      <button
+        v-if="!criar"
+        class="btn"
+        @click="criar=true"
+      >Criar conta</button>
+      <UsuarioForm v-else>
+        <button class="btn btn-form">Criar usuário</button>
+      </UsuarioForm>
+    </transition>
+  </section>
 </template>
 
 <script>
+import UsuarioForm from "@/components/UsuarioForm.vue"
 export default {
-  name: "LoginCriar"
+  name: "LoginCriar",
+  components: { UsuarioForm },
+  data () {
+    return {
+      criar: false
+    }
+  },
 }
 </script>
 
-<style>
+<style scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin: 0 auto;
+}
+
+.btn-form {
+  max-width: 100%;
+}
 </style>
